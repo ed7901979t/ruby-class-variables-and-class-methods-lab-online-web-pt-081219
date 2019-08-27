8423148c399cebcec627187b73f18c83ab088c5d
clean-command-1851 <ed7901979t@gmail.com>
2019-08-27 12:53:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,10 +6,16 @@ class Song
     @@count
   end 
   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end
    
   @@genres = []
-  
- 
   
   def self.genres
     @@genres.uniq
@@ -39,13 +45,6 @@ class Song
     @@artists.uniq
   end
   
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @@artists << artist
-    @genre = genre
-    @@genres << genre
-    @@count += 1
-  end
-  #attr_accessor :name, :artist, :genre
+   
+   
 end 
