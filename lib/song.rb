ecb08a82c1cdd59eb916791061a163b6122db348
clean-command-1851 <ed7901979t@gmail.com>
2019-08-27 12:47:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,4 +25,26 @@ class Album
   end
   
   @@genres = []
+  
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
+  
+  @@artists = []
+  
+  def self.artist_count
+    @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
 end
