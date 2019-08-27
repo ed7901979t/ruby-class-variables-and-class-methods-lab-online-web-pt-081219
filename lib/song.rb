a0c875e68ce038a28daf21ae7d43d692404c42c9
clean-command-1851 <ed7901979t@gmail.com>
2019-08-27 12:44:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,13 @@
 class Album
   @@album_count = 0 
   attr_accessor :name, :artist, :genre
-  def initialize 
-    @@album_count +=1   
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
   end
   
   def self.count    
